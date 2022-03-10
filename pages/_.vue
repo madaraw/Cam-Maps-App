a1346012e64b8145cd74470ececdf78d6b8a89e6
madaraw <rachediwalid@yahoo.fr>
2022-03-10 15:28:01 +0100

    added an app bar to the individual stream

--- pages/_.vue
+++ pages/_.vue
@@ -1,25 +1,38 @@
 <template>
-  <v-container class="screen pa-0 d-flex justify-center">
-    <NuxtLink to="/" class="back-btn"> <v-btn>Go back</v-btn> </NuxtLink>
-    <img :src="streamImg" class="stream" />
-  </v-container>
+  <v-main>
+    <v-app-bar >
+      <v-btn @click="handleGoBack">Go back</v-btn>
+      <v-toolbar-title>Ever- Public -cam</v-toolbar-title>
+    </v-app-bar>
+
+    <v-container class="screen pa-0 d-flex justify-center">
+      <img :src="streamImg" class="stream ma-0" />
+    </v-container>
+  </v-main>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      cameraId: this.$route.params.pathMatch.split('&')[0],
-      cameraFrequency: this.$route.params.pathMatch.split('&')[1],
-      streamImg: `https://media.evercam.io/v1/cameras/${this.$route.params.pathMatch.split('&')[0]}/thumbnail?thumbnailId=1`,
+      cameraId: this.$route.params.pathMatch.split("&")[0],
+      cameraFrequency: this.$route.params.pathMatch.split("&")[1],
+      streamImg: `https://media.evercam.io/v1/cameras/${
+        this.$route.params.pathMatch.split("&")[0]
+      }/thumbnail?thumbnailId=1`,
       refreshStreamImage: null,
     };
+  },
+  methods: {
+    handleGoBack() {
+      this.$router.push("/");
+    },
   },
   async mounted() {
     this.refreshStreamImage = setInterval(() => {
       let randomTime = new Date().getTime();
       this.streamImg = `https://media.evercam.io/v1/cameras/${this.cameraId}/thumbnail?thumbnailId=${randomTime}`;
-    }, 60/this.cameraFrequency *1000);
+    }, (60 / this.cameraFrequency) * 1000);
   },
   beforeDestroy() {
     clearInterval(this.refreshStreamImage);
@@ -30,17 +43,6 @@ export default {
 <style>
 .screen {
   height: 100vh;
-  width: 100vw;
-}
-.no-scroll::-webkit-scrollbar {
-  width: 0px;
-}
-.back-btn {
-  text-decoration: none;
-  position: absolute;
-  z-index: 10;
-  left: 0;
-  top: 0;
 }
 .stream {
   height: 100%;
